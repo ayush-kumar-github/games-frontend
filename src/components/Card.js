f01312c4588841aa9357e436f05db48c1aa41f0d
Ayush Kumar <aykr571@gmail.com>
2024-01-12 21:53:21 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({ product }) => {
+  return (
+    <div className="pb-3">
+      <img className="w-[100%] rounded h-80 sm:h-100" src={product.imageUrl} />
+      <h2 className="text-xl font-bold mb-2 p-3">{product.name}</h2>
+      <span className="text-gray-500 p-3">{product.brand}</span>
+      <div>
+        <span className="ml-2 p-1 mb-2 text-black-700 ">${product.price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
