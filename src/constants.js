ce108e805d22dd9a8e8871e6a5be1cbbd4da6340
Ayush Kumar <aykr571@gmail.com>
2024-01-14 11:56:58 +0530

    Ggggg7878

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
-export const BASE_URL = "https://games-backend-ctzu.vercel.app/";
+export const BASE_URL = "";
 export const UPLOADS_URL = "/api/upload";
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // app.use(
-  //   "/api",
-  //   createProxyMiddleware({
-  //     target: "https://games-backend-ctzu.vercel.app/",
-  //     changeOrigin: true,
-  //   })
-  // );
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://games-backend-ctzu.vercel.app/",
+      changeOrigin: true,
+    })
+  );
 };
